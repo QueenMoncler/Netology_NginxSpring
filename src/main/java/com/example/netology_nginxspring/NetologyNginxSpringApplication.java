ac2a90fffd28b43da6affd9f4de87f313a9bba32
queen <Queenmoncler@gmail.com>
2023-11-12 19:11:22 +0300

    Initial commit

---
+++ src/main/java/com/example/netology_nginxspring/NetologyNginxSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.netology_nginxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetologyNginxSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetologyNginxSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/netology_nginxspring/NetologyNginxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netology_nginxspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetologyNginxSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
